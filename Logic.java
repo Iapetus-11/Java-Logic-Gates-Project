4e7426a12270dd4ad436c4844398de3e47ea5348
Iapetus-11 <38477514+Iapetus-11@users.noreply.github.com>
2019-10-06 15:08:55 -0400

    Added more gates
    (2 input nand, 3 input nand, 2 input nor, 2 input xnor)

--- Logic.java
+++ Logic.java
@@ -1,5 +1,5 @@
 public class Logic{
-   //2 input and
+   //two input and
    public static boolean and2(boolean a, boolean b){
       return (a && b);
    }
@@ -7,6 +7,16 @@ public class Logic{
    //three input and
    public static boolean and3(boolean a, boolean b, boolean c){
       return (a && b && c);
+   }
+   
+   //two input nand
+   public static boolean nand2(boolean a, boolean b){
+      return !(a && b);
+   }
+   
+   //3 input nand
+   public static boolean nand3(boolean a, boolean b){
+      return !(a && b && c);
    }
    
    //two input or
@@ -19,9 +29,24 @@ public class Logic{
       return (a || b || c);
    }
    
-   //2 input xor
+   //two input nor
+   public static boolean nor2(boolean a, boolean b){
+      return !(a || b);
+   }
+   
+   //three input nor
+   public static boolean nor3(boolean a, boolean b, boolean c){
+      return !(a || b || c);
+   }
+   
+   //two input xor
    public static boolean xor2(boolean a, boolean b){
-      return (a && !b) || (!a && b);
+      return ((a && !b) || (!a && b));
+   }
+   
+   //two input xnor
+   public static boolean xnor2(boolean a, boolean b){
+      return ((a && b) || (!a && !b));
    }
    
    //inverter
