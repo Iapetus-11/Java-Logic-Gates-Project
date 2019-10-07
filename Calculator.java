4058a6a8ade8806777b1ab783dec2ec94fb1de61
Iapetus-11 <38477514+Iapetus-11@users.noreply.github.com>
2019-10-06 21:51:01 -0400

    Changed Logic class to L for ease of use

--- Calculator.java
+++ Calculator.java
@@ -10,22 +10,26 @@ public class Calculator{
       boolean db[] = new boolean[2];
       boolean difference = fullSubtractor(a0, b0, c0)[0];
       boolean borrow = fullSubtractor(a0, b0, c0)[1];
-      System.out.println("Difference: "+difference+", Borrow: "+borrow);      
+      System.out.println("Difference: "+difference+", Borrow: "+borrow);
    }
    
    //returns sum (index 0) and carry out (index 1)
    public static boolean[] fullAdder(boolean a, boolean b, boolean c){ //a and b are the addends (a + b), c is the carry number
       boolean sc[] = new boolean[2];
-      sc[0] = Logic.xor2(Logic.xor2(a, b), c); //sum
-      sc[1] = Logic.or3(Logic.and2(a, b), Logic.and2(a, c), Logic.and2(b, c)); //carry
+      sc[0] = L.xor2(L.xor2(a, b), c); //sum
+      sc[1] = L.or3(L.and2(a, b), L.and2(a, c), L.and2(b, c)); //carry
       return sc;
    }
    
    //returns difference (index 0) and borrow out (index 1)
    public static boolean[] fullSubtractor(boolean a, boolean b, boolean c){ //a is the minuend (a - x), b is the subtrahend (x - b), c is the borrow in
       boolean db[] = new boolean[2];
-      db[0] = Logic.xor2(Logic.xor2(a, b), c);
-      db[1] = Logic.or2(Logic.and2(Logic.invert(a), b), Logic.and2(Logic.invert(Logic.xor2(a, b)), c));
+      db[0] = L.xor2(L.xor2(a, b), c);
+      db[1] = L.or2(L.and2(L.invert(a), b), L.and2(L.invert(L.xor2(a, b)), c));
       return db;
    }
+   
+   /*public static boolean[] twoBitMultiplier(boolean a, boolean b){
+      
+   }*/
 }
---
+++ L.java
@@ -0,0 +1,58 @@
+public class L{
+   //two input and
+   public static boolean and2(boolean a, boolean b){
+      return (a && b);
+   }
+   
+   //three input and
+   public static boolean and3(boolean a, boolean b, boolean c){
+      return (a && b && c);
+   }
+   
+   //two input nand
+   public static boolean nand2(boolean a, boolean b){
+      return !(a && b);
+   }
+   
+   //3 input nand
+   public static boolean nand3(boolean a, boolean b, boolean c){
+      return !(a && b && c);
+   }
+   
+   //two input or
+   public static boolean or2(boolean a, boolean b){
+      return (a || b);
+   }
+   
+   //three input or
+   public static boolean or3(boolean a, boolean b, boolean c){
+      return (a || b || c);
+   }
+   
+   //two input nor
+   public static boolean nor2(boolean a, boolean b){
+      return !(a || b);
+   }
+   
+   //three input nor
+   public static boolean nor3(boolean a, boolean b, boolean c){
+      return !(a || b || c);
+   }
+   
+   //two input xor
+   public static boolean xor2(boolean a, boolean b){
+      return ((a && !b) || (!a && b));
+   }
+   
+   //two input xnor
+   public static boolean xnor2(boolean a, boolean b){
+      return ((a && b) || (!a && !b));
+   }
+   
+   //three input xor in xnor in future--maybe?
+   
+   //inverter
+   public static boolean invert(boolean a){
+      return (!a);
+   }
+}
